b45eb0109f300cf378295af4ab99881bfd80b871
aleonr171 <alejandro_1173@hotmail.com>
2024-10-29 13:06:49 +0100

    done

---
+++ src/app/auth/auth.directive.ts
@@ -0,0 +1,31 @@
+import {
+  Directive,
+  TemplateRef,
+  ViewContainerRef,
+  effect,
+  inject,
+  input,
+} from '@angular/core';
+import { Permission } from './auth.model';
+import { AuthService } from './auth.service';
+
+@Directive({
+  selector: '[appAuth]',
+  standalone: true,
+})
+export class AuthDirective {
+  userType = input.required<Permission>({ alias: 'appAuth' });
+  private readonly authService = inject(AuthService);
+  private readonly templateRef = inject(TemplateRef);
+  private readonly viewContainerRef = inject(ViewContainerRef);
+
+  constructor() {
+    effect(() => {
+      if (this.authService.activePermission() === this.userType()) {
+        this.viewContainerRef.createEmbeddedView(this.templateRef);
+      } else {
+        this.viewContainerRef.clear();
+      }
+    });
+  }
+}
